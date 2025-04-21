ff19b02fedcf6eaa82dad42ee450ed5df66a416b
mahan <babremeshki@mahan-macbook.local>
2025-04-21 15:34:51 +0330

    [Refactor] query.py - Limit function return to "data" section only

--- promql/query.py
+++ promql/query.py
@@ -4,6 +4,7 @@ def query_prometheus(prom_url, promql):
     response = requests.get(f"{prom_url}/api/v1/query", params={'query': promql})
     if response.status_code != 200:
         raise Exception(f"Error {response.status_code}: {response.text}")
-    return response.json()
+    json_response = response.json()
+    return json_response["data"]["result"]
 
 
