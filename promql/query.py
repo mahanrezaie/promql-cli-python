3a1e77ed0e536cb91650d769322212793ebebc29
mahan <babremeshki@mahan-macbook.local>
2025-04-21 13:49:48 +0330

    [Update] query.py - added query_prometheus func

---
+++ promql/query.py
@@ -0,0 +1,9 @@
+import requests
+
+def query_prometheus(prom_url, promql):
+    response = requests.get(f"{prom_url}/api/v1/query", params={'query': promql})
+    if response.status_code != 200:
+        raise Exception(f"Error {response.status_code}: {response.text}")
+    return response.json()
+
+
